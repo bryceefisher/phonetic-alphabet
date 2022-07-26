b466eba89e992babc5ef1bfd8f411d83b5886b99
Bryce Fisher <101945315+bryceefisher@users.noreply.github.com>
2022-07-25 19:10:33 -0700

    Update main.py

--- main.py
+++ main.py
@@ -4,15 +4,16 @@ with open("nato_phonetic_alphabet.csv", "r") as file:
     csv = csv.reader(file)
     nato_dict = dict(csv)
 
-user_input = input("Want to know the phonetic spelling of a word? Enter a word: ")
-indv_letters = list(user_input)
 
-phonetic_letters = []
+def generate_phon():
+    user_input = input("Want to know the phonetic spelling of a word? Enter a word: ")
+    try:
+        phonetic_letters = [nato_dict[letter.upper()] for letter in user_input]
+        print(phonetic_letters)
+    except KeyError:
+        print("Only enter letters, not numbers!")
+        generate_phon()
 
-for letter in indv_letters:
-    phonetic_letters.append(nato_dict[letter.upper()])
 
-print(phonetic_letters)
-
-
+generate_phon()
 
